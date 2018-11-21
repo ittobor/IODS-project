824877c2c0ece404b59e6cceadc7c7c2d5da23bb
ittobor <itt.obor@nbl.fi>
2018-11-21 21:09:17 +0200

    week4 exercise return

--- data/create_human.R
+++ data/create_human.R
@@ -13,7 +13,7 @@ gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2
 ##
 ##Step 3. Explore datasets
 ##
-
+# exploring with different functions
 str(hd)
 str(gii)
 dim(hd)
@@ -26,12 +26,15 @@ colnames(gii)
 ##
 ##Step 4. Rename columns for both dataframes
 ##
+# replacing current names with new names in string-vectors. Kept country name the same in both as it is used as join variable
+
 names(hd) <- c("HDI.rank", "country", "HDI.hdi", "HDI.lifeexpect","HDI.expeyearsedu","HDI,meanyearsedu","GDI.gni","HDI.gnirank")
 names(gii) <- c("GII.rank", "country", "GII.gii", "GII.matemortratio","GII.adobirthrate","GII.repinparperc","GII.secedupopF","GII.secedupopM","GII.labourrateF","GII.labourrateM")
 
 ##
 ##Step 5. Mutate "Gender inequality" dataset
 ##
+# mutating the gii-dataset with dplyr-library
 library(dplyr)
 gii <- mutate(gii, GII.seceduaratioFM = (GII.secedupopF / GII.secedupopM))
 gii <- mutate(gii, GII.labourrateratioFM = (GII.labourrateF / GII.labourrateM))
@@ -40,15 +43,20 @@ str(gii)
 ##
 ##Step 6. Join and save the dataset
 ##
+# joining the datasets with inner-join which keeps only data (countries) it can find from both datasets.
+# Then printing exploring the joined dataset
 human <- inner_join(hd, gii, by = "country")
 str(human)
 dim(human)
 summary(human)
 
-data_dir <- "C:/Users/ittobor/projects/iods/IODS-project/data"
+# Saving the dataset as file
+#data_dir <- "C:/Users/ittobor/projects/iods/IODS-project/data"
+data_dir <- "/Users/ittobor/studies/iods/IODS-project/data"
 out_data_name <- "human.csv"
 out_data_path <- paste(data_dir, out_data_name, sep="/")
 write.table(human, file=out_data_path, row.names = FALSE, sep = ";")
 
+# checking that the dimesions match
 human_check <- read.table(out_data_path, sep = ";" , header=TRUE)
 dim(human_check)
